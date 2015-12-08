816a787165a8b2d2f270384664d00c32a6d75d36
hothanhhung <thanhhung1012@gmail.com>
2015-12-08 16:58:19 +0700

    File Transfer: onclick on foldler
    click

--- TransferFile/Server/TransferFiles/app/src/main/resources/Website/js/app.js
+++ TransferFile/Server/TransferFiles/app/src/main/resources/Website/js/app.js
@@ -1,6 +1,21 @@
 angular.module('myApp', ['myApp.services'])
 .controller("listObjectControl", ['$scope', 'DataService', function ($scope, DataService){
-	$scope.directories = [];
-	$scope.directories = DataService.getDirectories('');
-                      
+	$scope.currentPath = '/';
+	$scope.parentPath = '';
+	$scope.dataInfo = DataService.getDirectories('');
+	
+    $scope.uploadFile = function(){
+        var file = $scope.myFile;
+        console.log('file is ' );
+        console.dir(file);
+        var uploadUrl = "/fileUpload";
+        fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+
+	$scope.getDirectories = function(path){
+		alert(path);
+		$scope.parentPath = $scope.currentPath;
+		$scope.currentPath = path;
+       // $scope.directories = DataService.getDirectories('');
+    };
 }]);
--- TransferFile/Server/TransferFiles/app/src/main/resources/Website/js/dataservice.js
+++ TransferFile/Server/TransferFiles/app/src/main/resources/Website/js/dataservice.js
@@ -1,22 +1,66 @@
+var myApp = angular.module('myApp.services', []);
+
+myApp.directive('fileModel', ['$parse', function ($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+                scope.$apply(function(){
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
+}]);
+
+myApp.service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+}]);
 angular.module('myApp.services', []).service('DataService', function(){
 	this.getDirectories = function(path){
+	/*
+	$http({
+	  method: 'GET',
+	  url: '/?path='+path
+	}).then(function successCallback(response) {
+		// this callback will be called asynchronously
+		// when the response is available
+	  }, function errorCallback(response) {
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	  });
+  */
   var data = [{
 				Name :'hung ',
-				FullPath:'hung dep trai',
+				FullPath:'hung dep trai1',
 				Size:"",
 				isFile: false ,
 				LastModified:'10-1-2003',
 			  },
 			  {
 				Name :'hung 2',
-				FullPath:'hung dep trai',
+				FullPath:'hung dep trai2',
 				Size:"14G",
 				isFile: false ,
 				LastModified:'23-2-2013',
 			  },
 			  {
 				Name :'hung 231',
-				FullPath:'hung dep trai',
+				FullPath:'hung dep trai3',
 				Size:"134B",
 				isFile: false ,
 				LastModified:'10-11-2014',
@@ -63,6 +107,10 @@ angular.module('myApp.services', []).service('DataService', function(){
 				isFile: true ,
 				LastModified:'10-11-2014',
 			  }];
-  return data;
+	result = {
+		error:false,
+		directories:data
+	}
+  return result;
   }
 });
