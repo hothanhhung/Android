53c66ff4cc30070cfc9cded35873a911e1a60813
hothanhhung <thanhhung1012@gmail.com>
2017-08-21 17:15:36 +0700

    Service: Add TiviOnline

---
+++ Services/TiviOnline/TiviOnline/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiviOnline.Bussiness;
+using TiviOnline.Models;
+
+namespace TiviOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(string channel = "VTV1", string id="")
+        {
+            if (string.IsNullOrWhiteSpace(channel)) channel = "VTV1";
+            var streamServers = DataJsonProcess.GetStreamServersOfChannel(channel);
+            var streamServer = DataJsonProcess.GetStreamServer(id);
+            if (streamServer == null && streamServers!= null && streamServers.Count > 0)
+            {
+                streamServer = streamServers.First();
+            }
+            ViewBag.StreamUrl = BussinessProcess.GetUrlStream(streamServer);
+            ViewBag.StreamServers = streamServers;
+
+            return View();
+        }
+
+        public ActionResult ChannelGroupsPartial()
+        {
+            ViewBag.HotChannels = DataJsonProcess.GetHotChannels();
+            ViewBag.VTV_VTCChannels = DataJsonProcess.GetVTV_VTCChannels();
+            ViewBag.SCTV_HTVChannels = DataJsonProcess.GetSCTV_HTVChannels();
+            ViewBag.ForeignChannels = DataJsonProcess.GetForeignChannels();
+            ViewBag.LocalChannels = DataJsonProcess.GetLocalChannels();
+            ViewBag.FootballChannels = DataJsonProcess.GetFootballChannels();
+            ViewBag.OthersChannels = DataJsonProcess.GetOthersChannels();
+            return PartialView();
+        }
+
+    }
+}
---
+++ Services/TiviOnline/TiviOnline/Models/StreamServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiviOnline.Models
+{
+    public class StreamServer
+    {
+        public string ID { get; set; }
+        public string URL { get; set; }
+        public int ServerId { get; set; }
+        public string ChannelId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
