126bd2d1e0fcb8cda03cd67f41387c560ceaa150
hothanhhung <thanhhung1012@gmail.com>
2017-10-02 17:15:35 +0700

    Service: Add QLBH Project

---
+++ Services/QLBH/QLBH/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QLBH.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId;
+        public int CategoryId;
+        public string ProductName;
+        public int PriceForSelling;
+        public string Unit;
+        public string Note;
+    }
+}
