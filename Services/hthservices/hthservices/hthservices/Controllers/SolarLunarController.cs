859b705ac1e98dae6765b975867d990fb8bce43c
hothanhhung <thanhhung1012@gmail.com>
2018-04-30 16:29:35 +0700

    hthService: update ThapNhiBatTu

--- Services/hthservices/hthservices/hthservices/Controllers/SolarLunarController.cs
+++ Services/hthservices/hthservices/hthservices/Controllers/SolarLunarController.cs
@@ -41,11 +41,22 @@ namespace hthservices.Controllers
         }
 
         [System.Web.Http.HttpGet]
-        public HttpResponseMessage GetInfoChamNgon(string date, int index)
+        public HttpResponseMessage GetGoodDateBadDateShort(string date, int index)
+        {
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(ThapNhiBatTuInfo.GetThapNhiBatTuShort(index), Encoding.UTF8, "text/html")
+            };
+        }
+
+        [System.Web.Http.HttpGet]
+        public HttpResponseMessage GetInfoChamNgon(string date, int index, int type = 0)
         {
             return new HttpResponseMessage()
             {
-                Content = new StringContent(ChamNgonInfo.GetThapNhiBatTuHTML(index), Encoding.UTF8, "text/html")
+                Content = type == 0? new StringContent(ChamNgonInfo.GetThapNhiBatTuHTML(index), Encoding.UTF8, "text/html") 
+                                    : new StringContent(ChamNgonInfo.GetThapNhiBatTuText(index), Encoding.UTF8, "text/html")
             };
          
         }
