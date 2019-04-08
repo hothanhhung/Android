caa0d4121569f09db8f87ef49914329d2ac47686
Andrew <andrew@clearpathdevelopment.com>
2019-04-08 15:49:53 +0700

    HaiChannel release v105

--- HaiChannel/MyApplication/app/src/main/java/com/hth/haitonghop/HomeActivity.java
+++ HaiChannel/MyApplication/app/src/main/java/com/hth/haitonghop/HomeActivity.java
@@ -1,9 +1,16 @@
 package com.hth.haitonghop;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
+/*import com.chartboost.sdk.CBLocation;
+import com.chartboost.sdk.Chartboost;
+import com.chartboost.sdk.ChartboostDelegate;
+import com.chartboost.sdk.Model.CBError;*/
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.hth.data.Data;
 import com.hth.data.ObjectChannel;
 import com.hth.data.YouTubeService;
@@ -24,6 +31,9 @@ import android.widget.LinearLayout;
 
 public class HomeActivity extends Activity {
 
+    private static long timeForRun = 0;
+    private int countShow = 0;
+    private InterstitialAd interstitial = null;
 	private AdView mAdView = null;
 	private static ArrayList<ObjectChannel> _lstChannels = null;
 	private ProgressDialog progressDialog = null;
@@ -37,6 +47,12 @@ public class HomeActivity extends Activity {
 			actionBar.setDisplayShowHomeEnabled(false);
 			actionBar.setDisplayShowTitleEnabled(false);
 		}
+		/*Chartboost.startWithAppId(this, "5c91a84d4d8f000d2a0579aa", "8026e22e4ac0e2f84d57abf1aa884bcd8f7b0564");
+		Chartboost.onCreate(this);
+		Chartboost.setDelegate(yourDelegateObject);
+		Chartboost.cacheInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+		Chartboost.cacheRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);*/
+
 		LinearLayout layoutEnglishChannels =(LinearLayout)findViewById(R.id.layoutEnglishChannels);
 		_lstChannels = Data.getChannels();
 		for(int i =0 ; i<_lstChannels.size(); i++)
@@ -46,13 +62,18 @@ public class HomeActivity extends Activity {
 			}
 			ObjectChannel channel = _lstChannels.get(i); 
 			Button button = new Button(this);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					ViewGroup.LayoutParams.MATCH_PARENT,
+					ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f);
+
+			params.setMargins(2, 5, 2, 5);
+			button.setLayoutParams(params);
+
 			button.setTextColor(Color.DKGRAY);
 			button.setText(channel.getTitle());
 			button.setTypeface(button.getTypeface(), Typeface.BOLD);
-			button.setTag(channel);			
-			button.setLayoutParams(new LinearLayout.LayoutParams(
-		        ViewGroup.LayoutParams.MATCH_PARENT,
-		            ViewGroup.LayoutParams.WRAP_CONTENT, 1.0f));
+			button.setTag(channel);
+
 			button.setOnClickListener(new View.OnClickListener() {
 				
 				@Override
@@ -74,6 +95,8 @@ public class HomeActivity extends Activity {
 	                startActivity(channelIntent);
 				}
 			});
+			button.setBackgroundColor(Color.CYAN);
+			button.setAlpha(0.9f);
 			layoutEnglishChannels.addView(button);
 		}
 
@@ -83,6 +106,75 @@ public class HomeActivity extends Activity {
 		mAdView.loadAd(adRequest);
 	}
 
+	private void showInterstitial()
+	{
+		long timenow = Calendar.getInstance().getTime().getTime();
+		/*long longtime = 350000;//(countShow*300000 );// + 100000;
+		//if(longtime > 1000000) longtime = 1000000;
+		if(timeForRun == 0){
+			timeForRun = Calendar.getInstance().getTime().getTime();
+		}
+		if((timenow - timeForRun) > longtime)
+		{
+			//StartAppAd.showAd(this);
+			if(Chartboost.hasRewardedVideo(CBLocation.LOCATION_HOME_SCREEN)){
+				Chartboost.showRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);
+				Chartboost.cacheRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);
+				timeForRun = Calendar.getInstance().getTime().getTime();
+			}
+			else if (Chartboost.hasInterstitial(CBLocation.LOCATION_HOME_SCREEN)) {
+				Chartboost.showInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+				Chartboost.cacheInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+				timeForRun = Calendar.getInstance().getTime().getTime();
+			}
+		}*/
+        timenow = Calendar.getInstance().getTime().getTime();
+        long maxTime = ((countShow*200000 ) + 200000);
+        if(maxTime > 400000) maxTime = 400000;
+        if(timeForRun > 0 && ((timenow - timeForRun) > maxTime))
+        {
+            if (interstitial == null) {
+                interstitial = new InterstitialAd(this);
+                interstitial.setAdUnitId(getResources().getString(R.string.ad_unit_id_interstitial));
+                interstitial.setAdListener(new AdListener() {
+                    @Override
+                    public void onAdLoaded() {
+                        if(interstitial.isLoaded()){
+                            interstitial.show();
+                            timeForRun = Calendar.getInstance().getTime().getTime();
+                            countShow++;
+                        }
+                    }
+
+                    @Override
+                    public void onAdClosed() {
+                    }
+
+
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                    }
+                });
+            }
+
+            AdRequest adRequest_interstitial = new AdRequest.Builder().build();
+
+            interstitial.loadAd(adRequest_interstitial);
+        }
+	}
+
+
+	/*private ChartboostDelegate yourDelegateObject = new ChartboostDelegate() {
+		// Declare delegate methods here, see CBSample project for examples
+		public void didFailToLoadInterstitial(String location, CBError.CBImpressionError error) {
+			Chartboost.cacheInterstitial(location);
+		}
+
+		public void didFailToLoadRewardedVideo(String location, CBError.CBImpressionError error) {
+			Chartboost.cacheRewardedVideo(location);
+		}
+	};
+*/
 	private void addMoreGameButton(LinearLayout layoutEnglishChannels) {
 		Button button = new Button(this);
 		button.setTextColor(Color.BLUE);
@@ -107,7 +199,7 @@ public class HomeActivity extends Activity {
 				UIUtils.showAlertGetMoreAppsServer(HomeActivity.this);
 			}
 		});
-		//button.setBackgroundColor(Color.WHITE);
+		button.setBackgroundColor(Color.CYAN);
 		button.setAlpha(0.98f);
 		layoutEnglishChannels.addView(button);
 	}
@@ -116,6 +208,7 @@ public class HomeActivity extends Activity {
     protected void onPause() {
         if(mAdView!=null) mAdView.pause();
         super.onPause();
+		//Chartboost.onPause(this);
     }
 
     @Override
@@ -126,11 +219,29 @@ public class HomeActivity extends Activity {
 			progressDialog.dismiss();
 		}
         if(mAdView!=null) mAdView.resume();
+		//Chartboost.onResume(this);
+		showInterstitial();
     }
 
     @Override
     protected void onDestroy() {
     	if(mAdView!=null) mAdView.destroy();
         super.onDestroy();
+		//Chartboost.onDestroy(this);
     }
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		//Chartboost.onStart(this);
+	}
+
+	@Override
+	public void onBackPressed() {
+		// If an interstitial is on screen, close it.
+		/*if (Chartboost.onBackPressed())
+			return;
+		else*/
+			super.onBackPressed();
+	}
 }
--- HaiChannel/MyApplication/app/src/main/java/com/hth/haitonghop/MainActivity.java
+++ HaiChannel/MyApplication/app/src/main/java/com/hth/haitonghop/MainActivity.java
@@ -2,6 +2,10 @@ package com.hth.haitonghop;
 
 import java.util.Calendar;
 
+/*import com.chartboost.sdk.CBLocation;
+import com.chartboost.sdk.Chartboost;
+import com.chartboost.sdk.ChartboostDelegate;
+import com.chartboost.sdk.Model.CBError;*/
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -39,6 +43,12 @@ public class MainActivity extends FragmentActivity implements		ActionBar.TabList
 
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 		setContentView(R.layout.activity_main);
+
+		/*Chartboost.startWithAppId(this, "5c91a84d4d8f000d2a0579aa", "8026e22e4ac0e2f84d57abf1aa884bcd8f7b0564");
+		Chartboost.onCreate(this);
+		Chartboost.setDelegate(yourDelegateObject);
+		Chartboost.cacheInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+		Chartboost.cacheRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);*/
 
 		if(!UIUtils.isOnline(this)){
         	UIUtils.showAlertErrorNoInternet(this, false);
@@ -82,34 +92,66 @@ public class MainActivity extends FragmentActivity implements		ActionBar.TabList
 			public void onPageScrollStateChanged(int arg0) {
 			}
 		});
-		timeForRun = Calendar.getInstance().getTime().getTime();
 		mAdView = (AdView) this.findViewById(R.id.adView);
 		AdRequest adRequest = new AdRequest.Builder().build();
 		mAdView.loadAd(adRequest);
 
 	}
 
-	
+	/*private ChartboostDelegate yourDelegateObject = new ChartboostDelegate() {
+		// Declare delegate methods here, see CBSample project for examples
+		public void didFailToLoadInterstitial(String location, CBError.CBImpressionError error) {
+			Chartboost.cacheInterstitial(location);
+		}
+
+		public void didFailToLoadRewardedVideo(String location, CBError.CBImpressionError error) {
+			Chartboost.cacheRewardedVideo(location);
+		}
+	};
+	*/
 	@Override
     protected void onPause() {
         if(mAdView!=null) mAdView.pause();
         super.onPause();
+	//	Chartboost.onPause(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+	//	Chartboost.onResume(this);
         showInterstitial();
 
     }
 
     private void showInterstitial()
     {
-        long timenow = Calendar.getInstance().getTime().getTime();
+		long timenow = Calendar.getInstance().getTime().getTime();
+		/*long longtime = 350000;//(countShow*300000 );// + 100000;
+		//if(longtime > 1000000) longtime = 1000000;
+		if(timeForRun == 0){
+			timeForRun = Calendar.getInstance().getTime().getTime();
+		}
+		if((timenow - timeForRun) > longtime)
+		{
+			//StartAppAd.showAd(this);
+			if(Chartboost.hasRewardedVideo(CBLocation.LOCATION_HOME_SCREEN)){
+				Chartboost.showRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);
+				Chartboost.cacheRewardedVideo(CBLocation.LOCATION_HOME_SCREEN);
+				timeForRun = Calendar.getInstance().getTime().getTime();
+			}
+			else if (Chartboost.hasInterstitial(CBLocation.LOCATION_HOME_SCREEN)) {
+				Chartboost.showInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+				Chartboost.cacheInterstitial(CBLocation.LOCATION_HOME_SCREEN);
+				timeForRun = Calendar.getInstance().getTime().getTime();
+			}
+		}*/
+
+		timenow = Calendar.getInstance().getTime().getTime();
 		long maxTime = ((countShow*200000 ) + 200000);
-		if(maxTime > 900000) maxTime = 900000;
-        if(timeForRun > 0 && ((timenow - timeForRun) > maxTime))
-        {
+		if(maxTime > 400000) maxTime = 400000;
+		if(timeForRun > 0 && ((timenow - timeForRun) > maxTime))
+		{
 			if (interstitial == null) {
 				interstitial = new InterstitialAd(this);
 				interstitial.setAdUnitId(getResources().getString(R.string.ad_unit_id_interstitial));
@@ -137,14 +179,32 @@ public class MainActivity extends FragmentActivity implements		ActionBar.TabList
 			AdRequest adRequest_interstitial = new AdRequest.Builder().build();
 
 			interstitial.loadAd(adRequest_interstitial);
-        }
+		}
     }
+
+	@Override
+	public void onBackPressed() {
+		/*if (Chartboost.onBackPressed())
+			return;
+		else */
+			{
+				super.onBackPressed();
+		}
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+	//	Chartboost.onStart(this);
+	}
+
     @Override
     protected void onDestroy() {
     	if(mAdView!=null) mAdView.destroy();
         super.onDestroy();
+	//	Chartboost.onDestroy(this);
     }
-	
+
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 	}
